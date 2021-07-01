b0a702c5bcfa225bfa7ee9892c49758467211913
Glenn <glenn@davy.net.au>
2021-07-01 21:47:42 +1200

    bundixed

---
+++ gemset.nix
@@ -0,0 +1,23 @@
+{
+  nokogiri = {
+    dependencies = ["racc"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1j42zavk76jd2844wibdan767v3gc1a2qppr61yw79ry87cncgf9";
+      type = "gem";
+    };
+    version = "1.11.7";
+  };
+  racc = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "178k7r0xn689spviqzhvazzvxfq6fyjldxb3ywjbgipbfi4s8j1g";
+      type = "gem";
+    };
+    version = "1.5.2";
+  };
+}
